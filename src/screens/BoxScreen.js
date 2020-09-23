1c2f55657cfe9c4b10f931edcede0ce10006c421
Márcio Vinícius <marcio.campos@outlook.com>
2020-09-23 09:38:50 -0300

    68. create BoxScreen

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>BoxScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black',
+  },
+  textStyle: {
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+});
+
+export default BoxScreen;
