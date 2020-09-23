4b6bbed03ff2f9d2e677af9b4ef4b31266fcfe38
Márcio Vinícius <marcio.campos@outlook.com>
2020-09-23 10:12:18 -0300

    73. Flex Values

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -5,7 +5,9 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>BoxScreen</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   );
 };
@@ -14,11 +16,22 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
     borderColor: 'black',
+    height: 200,
   },
-  textStyle: {
-    borderWidth: 10,
+  textOneStyle: {
+    borderWidth: 3,
     borderColor: 'red',
-    margin: 20,
+    flex: 4,
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    flex: 4,
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    flex: 2,
   },
 });
 
